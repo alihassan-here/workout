1b0ddf10ab18098c7b6d4c87f07d49d7d3d9e2f8
alihassan-here <alihassanuog50@gmail.com>
2022-07-17 19:02:59 +0500

    sign token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWT_SECRET, {
+        expiresIn: "3d",
+    });
+}
 
 const login = async (req, res) => {
     try {
@@ -11,7 +18,8 @@ const signup = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.signup(email, password);
-        res.status(200).json({ email, user })
+        const token = createToken(user._id);
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
