bc754a63fec83054bed9cb2700891c3a8130858a
alihassan-here <alihassanuog50@gmail.com>
2022-07-18 23:28:40 +0500

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,10 +8,13 @@ const createToken = (_id) => {
 }
 
 const login = async (req, res) => {
+    const { email, password } = req.body;
     try {
-        res.json("login route")
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.status(200).json({ email, token });
     } catch (error) {
-        console.log(error);
+        res.status(400).json({ error: error.message });
     }
 }
 const signup = async (req, res) => {
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -38,5 +38,24 @@ userSchema.statics.signup = async function (email, password) {
     return user;
 }
 
+//STATIC LOGIN METHOD
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error("All fields must be provided");
+    }
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid");
+    }
+    const user = await this.findOne({ email });
+    if (!user) {
+        throw Error("Email not found");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw Error("Password is incorrect");
+    }
+    return user;
+}
+
 
 module.exports = mongoose.model("User", userSchema);
